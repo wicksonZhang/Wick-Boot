d26810bddd0ba49a3e451eaa1830bdb2ab00ef85
DESKTOP-KRH17BU\minuo-java02 <wickson_zhang@163.com>
2024-04-08 19:21:19 +0800

    新增字典信息

---
+++ 02-security-system/src/main/java/cn/wickson/security/system/model/entity/SystemDictType.java
@@ -0,0 +1,53 @@
+package cn.wickson.security.system.model.entity;
+
+import cn.wickson.security.commons.enums.CommonStatusEnum;
+import cn.wickson.security.commons.model.entity.BaseDO;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.*;
+import lombok.experimental.Accessors;
+
+/**
+ * 系统管理 - 字典类型表
+ *
+ * @author ZhangZiHeng
+ * @date 2024-04-08
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@TableName("system_dict_type")
+public class SystemDictType extends BaseDO {
+
+    /**
+     * 字典主键
+     */
+    @TableId
+    private Long id;
+
+    /**
+     * 字典名称
+     */
+    private String name;
+
+    /**
+     * 字典类型
+     */
+    private String code;
+
+    /**
+     * 状态
+     * 枚举 {@link CommonStatusEnum}
+     */
+    private Integer status;
+
+    /**
+     * 备注
+     */
+    private String remark;
+
+}
