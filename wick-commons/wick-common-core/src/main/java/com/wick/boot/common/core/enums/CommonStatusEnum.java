34cdfb151fdc48dd54647c422c3a5f9cf55a4a88
DESKTOP-KRH17BU\minuo-java02 <wickson_zhang@163.com>
2024-04-15 16:46:36 +0800

    🚀【Task】新增枚举类型字段校验

--- wick-commons/wick-common-core/src/main/java/com/wick/boot/common/core/enums/CommonStatusEnum.java
+++ wick-commons/wick-common-core/src/main/java/com/wick/boot/common/core/enums/CommonStatusEnum.java
@@ -1,7 +1,10 @@
 package com.wick.boot.common.core.enums;
 
+import com.wick.boot.common.core.validator.IntArrayValuable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.Arrays;
 
 /**
  * 使用状态枚举
@@ -11,14 +14,21 @@ import lombok.Getter;
  */
 @Getter
 @AllArgsConstructor
-public enum CommonStatusEnum {
+public enum CommonStatusEnum implements IntArrayValuable {
 
     ENABLE(1, "启用"),
 
     DISABLE(0, "禁用");
 
+    public static final int[] ARRAYS = Arrays.stream(values()).mapToInt(CommonStatusEnum::getValue).toArray();
+
     private final Integer value;
 
     private final String label;
+
+    @Override
+    public int[] array() {
+        return ARRAYS;
+    }
 
 }
---
+++ wick-commons/wick-common-core/src/main/java/com/wick/boot/common/core/validator/IntArrayValuable.java
@@ -0,0 +1,13 @@
+package com.wick.boot.common.core.validator;
+
+/**
+ * 可生成 Int 数组的接口
+ */
+public interface IntArrayValuable {
+
+    /**
+     * @return int 数组
+     */
+    int[] array();
+
+}
