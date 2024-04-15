160ec2600d72dadab6a3c3af8ae2429f8cd9f3a0
DESKTOP-KRH17BU\minuo-java02 <wickson_zhang@163.com>
2024-04-15 14:12:35 +0800

    【task】统一修改包名

--- wick-sync-db-flyway/src/main/java/com/wick/boot/module/flyway/FlywayApplication.java
+++ wick-sync-db-flyway/src/main/java/com/wick/boot/module/flyway/FlywayApplication.java
@@ -1,4 +1,4 @@
-package com.wick.module.flyway;
+package com.wick.boot.module.flyway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
