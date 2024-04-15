82b224c55067af09c8f5afc62b5ca0f07dc99c58
DESKTOP-KRH17BU\minuo-java02 <wickson_zhang@163.com>
2024-04-15 11:30:45 +0800

    【task】优化 flyway 同步数据表信息

--- wick-admin-server/src/main/java/com/wick/server/WickServerApplication.java
+++ wick-admin-server/src/main/java/com/wick/server/WickServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author ZhangZiHeng
  * @date 2024-04-13
  */
-@SpringBootApplication(scanBasePackages = {"com.wick.server","com.wick.module"})
+@SpringBootApplication(scanBasePackages = {"com.wick.module"})
 public class WickServerApplication {
 
     public static void main(String[] args) {
