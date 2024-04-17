963ffd2df7c4f402cad37baac34a40dcf2f7e6d7
wicksonZhang <wickson_zhang@163.com>
2024-04-17 20:22:28 +0800

    🌱【task】新增接口-删除部门数据

--- wick-module-system/wick-module-system-boot/src/main/java/com/wick/boot/module/system/mapper/ISystemDeptMapper.java
+++ wick-module-system/wick-module-system-boot/src/main/java/com/wick/boot/module/system/mapper/ISystemDeptMapper.java
@@ -2,12 +2,15 @@ package com.wick.boot.module.system.mapper;
 
 import cn.hutool.core.util.ObjUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.wick.boot.common.core.enums.CommonStatusEnum;
 import com.wick.boot.common.mybatis.mapper.BaseMapperX;
 import com.wick.boot.module.system.model.entity.SystemDept;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author ZhangZiHeng
@@ -57,10 +60,26 @@ public interface ISystemDeptMapper extends BaseMapperX<SystemDept> {
         );
     }
 
-    default List<SystemDept> selectDeptByIdOrTreePath(List<Long> ids) {
-        return this.selectList(new LambdaQueryWrapper<SystemDept>()
-                .in(SystemDept::getId, ids).or()
-                .apply("CONCAT (',',tree_path,',') LIKE CONCAT('%,',{0},',%')", ids)
-        );
+    /**
+     * 根据部门id模糊查询
+     *
+     * @param ids 部门集合ids
+     * @return List<SystemDept> 部门集合数据
+     */
+    default Set<SystemDept> selectDeptByIdOrTreePath(List<Long> ids) {
+        // 模糊匹配查询
+        // SELECT id,name,parent_id,tree_path,sort,status,create_time,update_time,create_by,update_by,deleted
+        // FROM system_dept
+        // WHERE deleted=0 AND (id = ? OR CONCAT(',', tree_path, ',') LIKE CONCAT('%,', ?, ',%'))
+        List<SystemDept> resultList = Lists.newArrayList();
+        ids.forEach(id -> {
+            List<SystemDept> byTreePath = this.selectList(new LambdaQueryWrapper<SystemDept>()
+                    .eq(SystemDept::getId, id)
+                    .or()
+                    .apply("CONCAT(',', tree_path, ',') LIKE CONCAT('%,', {0}, ',%')", id)
+            );
+            resultList.addAll(byTreePath);
+        });
+        return Sets.newHashSet(resultList);
     }
 }
