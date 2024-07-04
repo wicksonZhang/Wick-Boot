1897bb395f98250393851b40c77fffa91fe8bced
wicksonZhang <wickson_zhang@163.com>
2024-07-04 20:34:39 +0800

    🌱【task】认证中心-优化获取验证码接口

--- wick-module-auth/src/main/java/com/wick/boot/module/auth/service/impl/AuthServiceImpl.java
+++ wick-module-auth/src/main/java/com/wick/boot/module/auth/service/impl/AuthServiceImpl.java
@@ -64,12 +64,9 @@ public class AuthServiceImpl implements IAuthService {
         /* Step-2: 生成验证码信息 */
         GifCaptcha captcha =
                 CaptchaUtil.createGifCaptcha(CaptchaConstants.CAPTCHA_WIDTH, CaptchaConstants.CAPTCHA_HEIGHT, CaptchaConstants.CAPTCHA_COUNT);
-        System.out.println("captcha = " + captcha);
         /* Step-3: 将验证码存入redis */
         String captchaKey = IdUtil.fastSimpleUUID();
         String redisKey = GlobalCacheConstants.getCaptchaCodeKey(captchaKey);
-        System.out.println("redisService = " + redisService);
-        System.out.println("captcha.getCode() = " + captcha.getCode());
         redisService.setCacheObject(redisKey, captcha.getCode(), CaptchaConstants.CAPTCHA_TIME_OUT, TimeUnit.SECONDS);
 
         /* Step-4: 返回结果集 */
