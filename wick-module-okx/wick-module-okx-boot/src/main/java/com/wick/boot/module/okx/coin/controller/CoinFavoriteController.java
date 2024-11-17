d4f716d3eb0db7fcad1b2f6e9357576cd159c3a7
MM-202201031723\Lenovo <zhangziheng@joysfintech.com>
2024-11-17 21:47:06 +0800

    【Task】新增 okx 模块

---
+++ wick-module-okx/wick-module-okx-boot/src/main/java/com/wick/boot/module/okx/coin/controller/CoinFavoriteController.java
@@ -0,0 +1,20 @@
+package com.wick.boot.module.okx.coin.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 我的自选-Controller
+ *
+ * @author Wickson
+ * @date 2024-11-17 21:13
+ **/
+@RestController
+@RequestMapping("/coin/favorite")
+@Api(value = "/coin/favorite", tags = {"04-我的自选"})
+public class CoinFavoriteController {
+
+
+
+}
